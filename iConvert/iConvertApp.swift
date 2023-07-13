313135cf3dbc31217fda7bd91321a133914538bb
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-07-13 17:28:02 +0300

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Oleg Gavashi on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
